00c1d2497255da4571cf6a794655d9c1b8c7a460
Klop <Klop@DESKTOP-K8PVHR8>
2026-03-03 16:12:55 +0300

    Add project files.

---
+++ Clock/MainForm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clock
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            labelTime.Text = DateTime.Now.ToString("HH:mm:ss");
+            if (cbShowDate.Checked)
+                labelTime.Text += $"\n{DateTime.Now.ToString("yyyy:MM:dd")}";
+            if (cbShowDate.Checked)
+                labelTime.Text += $"\n{DateTime.Now.DayOfWeek}";
+        
+        }
+
+        private void btnHideControls_Click(object sender, EventArgs e)
+        {
+            cbShowDate.Visible = false;
+            cbShowWeekday.Visible = false;
+            btnHideControls.Visible = false;
+            ShowInTaskbar = false;
+        }
+    }
+}
